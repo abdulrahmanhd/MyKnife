fffb7b77bcfb4722e5af8f9e7c8658b4de8f462b
developer-eslam <eslamabotaleb12@hotmail.com>
2017-04-15 20:43:08 +0200

    Initial commit

---
+++ app/src/main/java/com/serverproject/facedetection/myknife/MainActivity.java
@@ -0,0 +1,28 @@
+package com.serverproject.facedetection.myknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    
+    @OnClick(R.id.btnshow)
+   public  void setbtnshow(){
+        Toast.makeText(MainActivity.this,"Message",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
